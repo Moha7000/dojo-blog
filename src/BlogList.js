5c2c1d6b885011789e53cda061e69bf22fc31062
unknown <moha.hossam96@gmail.com>
2021-07-12 09:11:13 +0200

    added date of creation for blogs

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,19 +1,21 @@
 import { Link } from "react-router-dom";
 
 const BlogList = ({ blogs, title }) => {
-    return ( 
-        <div className="blog-list">
-        <h2>{title}</h2>
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <Link to={`/blogs/${blog.id}`}>
-                        <h2>{blog.title}</h2>
-                        <p>Written by {blog.author}</p>
-                    </Link>
-                </div>
-             ))}
-       </div>
-     );
-}
- 
+  return (
+    <div className="blog-list">
+      <h2>{title}</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <Link to={`/blogs/${blog.id}`}>
+            <h2>{blog.title}</h2>
+            <p className="blog-date"> {blog.date.slice(0, 10)} </p>
+            <p className="blog-author">Written by {blog.author} </p>
+            <p>{blog.body.slice(0, 150)}...</p>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
 export default BlogList;
